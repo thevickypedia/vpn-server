d86484f1dffd8020bfb677d7eb30c0a0daf85772
thevickypedia <vignesh.sivanandarao@gmail.com>
2021-09-14 21:19:40 -0500

    Add logging instead of print statements

--- vpn.py
+++ vpn.py
@@ -1,8 +1,15 @@
+from logging import getLogger, basicConfig, INFO
 from os import environ
 
 from boto3 import client
 
 key_name = 'OpenVPN'
+
+basicConfig(
+    format='%(asctime)s - %(levelname)s - [%(module)s:%(lineno)d] - %(funcName)s - %(message)s',
+    datefmt='%b-%d-%Y %I:%M:%S %p', level=INFO
+)
+logger = getLogger(key_name)
 
 ec2_client = client(service_name='ec2', region_name='us-west-2')
 
@@ -13,17 +20,19 @@ def _create_key_pair():
         KeyType='rsa'
     )
     if key_response.get('ResponseMetadata').get('HTTPStatusCode') == 200:
-        print(f'Successfully created a key pair named: {key_name}')
+        logger.info(f'Successfully created a key pair named: {key_name}')
         with open(f'{key_name}.pem', 'w') as file:
             file.write(key_response.get('KeyMaterial'))
-        print(f'Stored the certificate as {key_name}.pem')
+        logger.info(f'Stored the certificate as {key_name}.pem')
         return True
     else:
-        exit(f'Unable to create a key pair: {key_name}')
+        logger.error(f'Unable to create a key pair: {key_name}')
 
 
 def create_ec2_instance():
-    _create_key_pair()
+    if not _create_key_pair():
+        return
+
     ec2_response = ec2_client.run_instances(
         InstanceType="t2.micro",
         MaxCount=1,
@@ -33,10 +42,10 @@ def create_ec2_instance():
     )
     if ec2_response.get('ResponseMetadata').get('HTTPStatusCode') == 200:
         instance_id = ec2_response.get('Instances')[0].get('InstanceId')
-        print(f'Created the EC2 instance: {instance_id}')
+        logger.info(f'Created the EC2 instance: {instance_id}')
         return instance_id
     else:
-        exit('Failed to create an EC2 instance.')
+        logger.error('Failed to create an EC2 instance.')
 
 
 if __name__ == '__main__':
