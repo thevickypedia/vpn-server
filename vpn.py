2e6d24d442acc57d19befbf5fd88ef5ec5d7fa5d
thevickypedia <vignesh.sivanandarao@gmail.com>
2021-09-14 21:17:26 -0500

    Create pem file while spinning up an instance

--- vpn.py
+++ vpn.py
@@ -1,23 +1,43 @@
 from os import environ
 
 from boto3 import client
-from botocore.exceptions import ClientError
 
-ec2_client = client('ec2')
+key_name = 'OpenVPN'
 
-try:
+ec2_client = client(service_name='ec2', region_name='us-west-2')
+
+
+def _create_key_pair():
+    key_response = ec2_client.create_key_pair(
+        KeyName=key_name,
+        KeyType='rsa'
+    )
+    if key_response.get('ResponseMetadata').get('HTTPStatusCode') == 200:
+        print(f'Successfully created a key pair named: {key_name}')
+        with open(f'{key_name}.pem', 'w') as file:
+            file.write(key_response.get('KeyMaterial'))
+        print(f'Stored the certificate as {key_name}.pem')
+        return True
+    else:
+        exit(f'Unable to create a key pair: {key_name}')
+
+
+def create_ec2_instance():
+    _create_key_pair()
     ec2_response = ec2_client.run_instances(
         InstanceType="t2.micro",
         MaxCount=1,
         MinCount=1,
-        ImageId=environ.get('ami_id')
+        ImageId=environ.get('ami_id'),
+        KeyName=key_name
     )
-except ClientError as error:
-    ec2_response = None
-    exit(f'Run instance call failed with the message:\n{error}')
+    if ec2_response.get('ResponseMetadata').get('HTTPStatusCode') == 200:
+        instance_id = ec2_response.get('Instances')[0].get('InstanceId')
+        print(f'Created the EC2 instance: {instance_id}')
+        return instance_id
+    else:
+        exit('Failed to create an EC2 instance.')
 
-if ec2_response.get('ResponseMetadata').get('HTTPStatusCode') == 200:
-    instance_id = ec2_response.get('Instances')[0].get('InstanceId')
-    print(instance_id)
-else:
-    print('Failed to spin up an instance.')
+
+if __name__ == '__main__':
+    create_ec2_instance()
