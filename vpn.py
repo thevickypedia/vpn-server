f252c4a4101e25f091eeab7df9001b2fd7a51ddc
thevickypedia <vignesh.sivanandarao@gmail.com>
2021-09-14 22:33:13 -0500

    Add exception handlers where necessary

--- vpn.py
+++ vpn.py
@@ -4,6 +4,7 @@ from os import environ, path, system
 from time import sleep
 
 from boto3 import client, resource
+from botocore.exceptions import ClientError
 
 key_name = 'OpenVPN'
 
@@ -18,14 +19,19 @@ ec2_resource = resource(service_name='ec2', region_name='us-west-2')
 
 
 def _create_key_pair() -> bool:
-    key_response = ec2_client.create_key_pair(
-        KeyName=key_name,
-        KeyType='rsa'
-    )
-    if key_response.get('ResponseMetadata').get('HTTPStatusCode') == 200:
+    try:
+        response = ec2_client.create_key_pair(
+            KeyName=key_name,
+            KeyType='rsa'
+        )
+    except ClientError as error:
+        logger.error(f'API call to create key pair has failed.\n{error}')
+        return False
+
+    if response.get('ResponseMetadata').get('HTTPStatusCode') == 200:
         logger.info(f'Successfully created a key pair named: {key_name}')
         with open(f'{key_name}.pem', 'w') as file:
-            file.write(key_response.get('KeyMaterial'))
+            file.write(response.get('KeyMaterial'))
         logger.info(f'Stored the certificate as {key_name}.pem')
         return True
     else:
@@ -36,15 +42,20 @@ def create_ec2_instance() -> str or None:
     if not _create_key_pair():
         return
 
-    ec2_response = ec2_client.run_instances(
-        InstanceType="t2.micro",
-        MaxCount=1,
-        MinCount=1,
-        ImageId=environ.get('ami_id'),
-        KeyName=key_name
-    )
-    if ec2_response.get('ResponseMetadata').get('HTTPStatusCode') == 200:
-        instance_id = ec2_response.get('Instances')[0].get('InstanceId')
+    try:
+        response = ec2_client.run_instances(
+            InstanceType="t2.micro",
+            MaxCount=1,
+            MinCount=1,
+            ImageId=environ.get('ami_id'),
+            KeyName=key_name
+        )
+    except ClientError as error:
+        logger.error(f'API call to create instance has failed.\n{error}')
+        return
+
+    if response.get('ResponseMetadata').get('HTTPStatusCode') == 200:
+        instance_id = response.get('Instances')[0].get('InstanceId')
         logger.info(f'Created the EC2 instance: {instance_id}')
         return instance_id
     else:
@@ -52,9 +63,14 @@ def create_ec2_instance() -> str or None:
 
 
 def _delete_key_pair(target_key: str = key_name) -> bool:
-    response = ec2_client.delete_key_pair(
-        KeyName=target_key
-    )
+    try:
+        response = ec2_client.delete_key_pair(
+            KeyName=target_key
+        )
+    except ClientError as error:
+        logger.error(f'API call to delete the key {key_name} has failed.\n{error}')
+        return False
+
     if response.get('ResponseMetadata').get('HTTPStatusCode') == 200:
         logger.info(f'{key_name} has been deleted from KeyPairs.')
         return True
@@ -65,9 +81,15 @@ def _delete_key_pair(target_key: str = key_name) -> bool:
 def terminate_ec2_instance(instance_id: str) -> None:
     if not _delete_key_pair():
         return
-    response = ec2_client.terminate_instances(
-        InstanceIds=[instance_id]
-    )
+
+    try:
+        response = ec2_client.terminate_instances(
+            InstanceIds=[instance_id]
+        )
+    except ClientError as error:
+        logger.error(f'API call to terminate the instance has failed.\n{error}')
+        return
+
     if response.get('ResponseMetadata').get('HTTPStatusCode') == 200:
         logger.info(f'InstanceId {instance_id} has been set to terminate.')
         if path.exists(f'{key_name}.pem'):
@@ -78,16 +100,21 @@ def terminate_ec2_instance(instance_id: str) -> None:
         logger.error(f'Failed to terminate the InstanceId: {instance_id}')
 
 
-def _get_public_dns(instance_id: str) -> tuple:
+def _get_public_dns(instance_id: str) -> tuple or None:
     logger.info('Waiting for the instance to go live.')
     while True:
-        info_response = ec2_client.describe_instance_status(
-            InstanceIds=[instance_id]
-        )
-        if info_response.get('ResponseMetadata').get('HTTPStatusCode') != 200:
+        try:
+            response = ec2_client.describe_instance_status(
+                InstanceIds=[instance_id]
+            )
+        except ClientError as error:
+            logger.error(f'API call to describe instance has failed.{error}')
+            return
+
+        if response.get('ResponseMetadata').get('HTTPStatusCode') != 200:
             sleep(3)
             continue
-        if status := info_response.get('InstanceStatuses'):
+        if status := response.get('InstanceStatuses'):
             if status[0].get('InstanceState').get('Name') == 'running':
                 instance_info = ec2_resource.Instance(instance_id)
                 return instance_info.public_dns_name, instance_info.public_ip_address
@@ -98,8 +125,8 @@ def _get_public_dns(instance_id: str) -> tuple:
 
 
 def configure_openvpn() -> None:
-    if instance_id := create_ec2_instance():
-        public_dns, public_ip = _get_public_dns(instance_id=instance_id)
+    if (instance_id := create_ec2_instance()) and (tuple_object := _get_public_dns(instance_id=instance_id)):
+        public_dns, public_ip = tuple_object
 
         instance_info = {
             'instance_id': instance_id,
