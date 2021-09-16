cac443ce2eaf268a6068215627948fb527d3e130
thevickypedia <vignesh.sivanandarao@gmail.com>
2021-09-16 01:37:59 -0500

    FEATURE::Spins up a VPN Server on EC2 with a single click Add all the automation bits Add time converter to calculate run time Add an apple script for the automation Add functionality to re-use AWS resources

--- docs/_sources/README.md.txt
+++ docs/_sources/README.md.txt
@@ -1,20 +1,46 @@
-# VPN Server
-Automate on demand VPN Server creation running with OpenVPN using AWS EC2 and Python
+<br>
 
-### Instructions
-#### Setup
+[![Python 3.8](https://img.shields.io/badge/python-3.8-orange.svg)](https://www.python.org/downloads/release/python-385/)
+[![Python 3.9](https://img.shields.io/badge/python-3.9-blue.svg)](https://www.python.org/downloads/release/python-391/)
+
+###### Platform Supported
+![Generic badge](https://img.shields.io/badge/Platform-MacOS-1f425f.svg)
+
+###### Repo Stats
+[![GitHub](https://img.shields.io/github/license/thevickypedia/vpn-server)](https://github.com/thevickypedia/vpn-server/blob/main/LICENSE)
+[![GitHub repo size](https://img.shields.io/github/repo-size/thevickypedia/vpn-server)](https://api.github.com/repos/thevickypedia/vpn-server)
+[![GitHub code size](https://img.shields.io/github/languages/code-size/thevickypedia/vpn-server)](https://api.github.com/repos/thevickypedia/vpn-server)
+[![LOC](https://img.shields.io/tokei/lines/github/thevickypedia/vpn-server)](https://api.github.com/repos/thevickypedia/vpn-server)
+
+###### Deployments
+[![docs](https://github.com/thevickypedia/vpn-server/actions/workflows/docs.yml/badge.svg)](https://thevickypedia.github.io/vpn-server/)
+
+# VPN Server
+On demand VPN Server creation running with `OpenVPN` using `AWS EC2` and `Python`.
+
+Got 5 minutes to spare? Spin up your own VPN server on demand.
+
+### Setup and Configuration
 1. `git clone https://github.com/thevickypedia/vpn-server.git`
 2. `python3 -m venv venv`
 3. `source venv/bin/activate`
-4. `pip3 install -r requirements.txt`
-5. `python3 vpn.py`
+4. `pip install -r requirements.txt`
+5. `export ami_id=ami-0e21cddg3k0c9a930 vpn_password=awsOpenVPN2021` - Sample
+6. `python vpn.py`
+   - Call the class method:
+     - `startup_vpn` to initiate the `VPN Server`
+     - `shutdown_vpn` to delete all resource spun up for the `VPN Server`
+   - `Runtime: ~3 minutes`
 
-#### Configuration
-1. Run the first command printed in `STDOUT`.
-2. Accept the agreement when prompted.
-   - `Please enter 'yes' to indicate your agreement [no]: `
-3. Hit `Enter/Return` for rest of the prompts.
-4. Run the second command printed in `STDOUT` to finish the configuration.
+### AWS Resources Used
+- EC2
+  - Instances
+  - AMI
+  - KeyPairs
+  - SecurityGroups
+- Network Interfaces
+- VPC [Default]
+- Subnet [Default]
 
 ### Linting
 `PreCommit` will ensure linting, and the doc creation are run on every commit.
@@ -26,6 +52,11 @@ Requirement:
 Usage:
 <br>
 `pre-commit run --all-files`
+
+### Limitations
+- Currently, runs only on `MacOS`
+- Can be run only from a local `Macbook` or `iMac`
+- There is an over provisioned waiter time running for few AWS interactions.
 
 ### Links
 [Repository](https://github.com/thevickypedia/vpn-server)
--- vpn.py
+++ vpn.py
@@ -1,12 +1,38 @@
 from json import dump, load
 from logging import INFO, basicConfig, getLogger
-from os import environ, path, system
+from os import environ, getcwd, path, system
 from subprocess import STDOUT, CalledProcessError, check_output
 from sys import stdout
-from time import sleep
+from time import perf_counter, sleep
 
 from boto3 import client, resource
 from botocore.exceptions import ClientError
+
+
+def time_converter(seconds: float) -> str:
+    """Modifies seconds to appropriate days/hours/minutes/seconds.
+
+    Args:
+        seconds: Takes number of seconds as argument.
+
+    Returns:
+        str:
+        Seconds converted to days or hours or minutes or seconds.
+    """
+    days = round(seconds // 86400)
+    seconds = round(seconds % (24 * 3600))
+    hours = round(seconds // 3600)
+    seconds %= 3600
+    minutes = round(seconds // 60)
+    seconds %= 60
+    if days:
+        return f'{days} days, {hours} hours, {minutes} minutes, and {seconds} seconds'
+    elif hours:
+        return f'{hours} hours, {minutes} minutes, and {seconds} seconds'
+    elif minutes:
+        return f'{minutes} minutes, and {seconds} seconds'
+    elif seconds:
+        return f'{seconds} seconds'
 
 
 class VPNServer:
@@ -27,13 +53,19 @@ class VPNServer:
             - If no values are passed during object initialization, script checks for environment variables.
             - If the environment variables are ``null``, gets the default credentials from ``~/.aws/credentials``.
         """
+        # Hard-coded certificate file name, server information file name, security group name
         self.key_name = 'OpenVPN'
         self.server_file = 'server_info.json'
+        self.security_group_name = 'OpenVPN Access Server'
+
+        # Logger setup
         basicConfig(
             format='%(asctime)s - %(levelname)s - [%(module)s:%(lineno)d] - %(funcName)s - %(message)s',
             datefmt='%b-%d-%Y %I:%M:%S %p', level=INFO
         )
         self.logger = getLogger(self.key_name)
+
+        # AWS client and resource setup
         if (access_key := environ.get('access_key', aws_access_key)) and \
                 (secret_key := environ.get('secret_key', aws_secret_key)):
             self.ec2_client = client(service_name='ec2', region_name='us-west-2',
@@ -43,6 +75,10 @@ class VPNServer:
         else:
             self.ec2_client = client(service_name='ec2', region_name='us-west-2')
             self.ec2_resource = resource(service_name='ec2', region_name='us-west-2')
+
+    def __del__(self):
+        """Destructor to print the run time at the end."""
+        self.logger.info(f'Total runtime: {time_converter(perf_counter())}')
 
     def _create_key_pair(self) -> bool:
         """Creates a ``KeyPair`` of type ``RSA`` stored as a ``PEM`` file to use with ``OpenSSH``.
@@ -57,6 +93,12 @@ class VPNServer:
                 KeyType='rsa'
             )
         except ClientError as error:
+            error = str(error)
+            if '(InvalidKeyPair.Duplicate)' in error and self.key_name in error:
+                self.logger.warning(f'Found an existing KeyPair named: {self.key_name}. Re-creating it.')
+                self._delete_key_pair()
+                self._create_key_pair()
+                return True
             self.logger.error(f'API call to create key pair has failed.\n{error}')
             return False
 
@@ -125,6 +167,10 @@ class VPNServer:
                      'IpRanges': [{'CidrIp': '0.0.0.0/0'}]}
                 ])
         except ClientError as error:
+            error = str(error)
+            if '(InvalidPermission.Duplicate)' in error:
+                self.logger.warning(f'Identified same permissions in an existing SecurityGroup: {security_group_id}')
+                return True
             self.logger.error(f'API call to authorize the security group {security_group_id} has failed.\n{error}')
             return False
         if response.get('ResponseMetadata').get('HTTPStatusCode') == 200:
@@ -152,11 +198,21 @@ class VPNServer:
 
         try:
             response = self.ec2_client.create_security_group(
-                GroupName='OpenVPN Access Server',
+                GroupName=self.security_group_name,
                 Description='Security Group to allow certain port ranges for VPN server.',
                 VpcId=vpc_id
             )
         except ClientError as error:
+            error = str(error)
+            if '(InvalidGroup.Duplicate)' in error and self.security_group_name in error:
+                self.logger.warning(f'Found an existing SecurityGroup named: {self.security_group_name}. Reusing it.')
+                response = self.ec2_client.describe_security_groups(
+                    Filters=[
+                        dict(Name='group-name', Values=[self.security_group_name])
+                    ]
+                )
+                group_id = response['SecurityGroups'][0]['GroupId']
+                return group_id
             self.logger.error(f'API call to create security group has failed.\n{error}')
             return
 
@@ -225,11 +281,15 @@ class VPNServer:
             data = load(file)
         return data
 
-    def _delete_key_pair(self, key_name: str = None):
+    def _delete_key_pair(self, key_name: str = None) -> bool:
         """Deletes the ``KeyPair``.
 
         Args:
             key_name: Takes ``KeyPair`` name as argument. Defaults to the one mentioned when the object was initialized.
+
+        Returns:
+            bool:
+            Flag to indicate the calling function if or not the KeyPair was deleted.
         """
         if not key_name:
             key_name = self.key_name
@@ -241,25 +301,30 @@ class VPNServer:
             )
         except ClientError as error:
             self.logger.error(f'API call to delete the key {self.key_name} has failed.\n{error}')
-            return
+            return False
 
         if response.get('ResponseMetadata').get('HTTPStatusCode') == 200:
             self.logger.info(f'{self.key_name} has been deleted from KeyPairs.')
             if path.exists(f'{self.key_name}.pem'):
                 system(f'rm {self.key_name}.pem')
+            return True
         else:
             self.logger.error(f'Failed to delete the key: {self.key_name}')
 
-    def _delete_security_group(self, security_group_id: str = None) -> None:
+    def _delete_security_group(self, security_group_id: str = None) -> bool:
         """Deletes the security group.
 
         Args:
             security_group_id: Takes the SecurityGroup ID as an argument.
+
+        Returns:
+            bool:
+            Flag to indicate the calling function if or not the SecurityGroup was deleted.
         """
         if not security_group_id:
             if not path.exists(self.server_file):
                 self.logger.error('Cannot delete a security group without the SecurityGroup ID')
-                return
+                return False
 
             data = self._retrieve_server_info()
             security_group_id = data.get('security_group_id')
@@ -271,23 +336,28 @@ class VPNServer:
             )
         except ClientError as error:
             self.logger.error(f'API call to delete the Security Group {security_group_id} has failed.\n{error}')
-            return
+            return False
 
         if response.get('ResponseMetadata').get('HTTPStatusCode') == 200:
             self.logger.info(f'{security_group_id} has been deleted from Security Groups.')
+            return True
         else:
             self.logger.error(f'Failed to delete the SecurityGroup: {security_group_id}')
 
-    def _terminate_ec2_instance(self, instance_id: str = None) -> None:
+    def _terminate_ec2_instance(self, instance_id: str = None) -> bool:
         """Terminates the requested instance.
 
         Args:
             instance_id: Takes instance ID as an argument. Defaults to the instance that was created previously.
+
+        Returns:
+            bool:
+            Flag to indicate the calling function if or not the instance was terminated.
         """
         if not instance_id:
             if not path.exists(self.server_file):
                 self.logger.error('Cannot terminate an instance without the Instance ID')
-                return
+                return False
 
             data = self._retrieve_server_info()
             instance_id = data.get('instance_id')
@@ -299,10 +369,11 @@ class VPNServer:
             )
         except ClientError as error:
             self.logger.error(f'API call to terminate the instance has failed.\n{error}')
-            return
+            return False
 
         if response.get('ResponseMetadata').get('HTTPStatusCode') == 200:
             self.logger.info(f'InstanceId {instance_id} has been set to terminate.')
+            return True
         else:
             self.logger.error(f'Failed to terminate the InstanceId: {instance_id}')
 
@@ -342,7 +413,7 @@ class VPNServer:
         """Automation to add the the ``host``, ``ip`` and digital signature to the ``known_hosts`` file.
 
         See Also:
-            There is a waiter running for 20 seconds to honor DNS propagation time.
+            There is a waiter for 20 seconds to honor DNS propagation time.
 
         Args:
             public_ip: Public IP address of an instance.
@@ -352,12 +423,11 @@ class VPNServer:
             bool:
             Flag to indicate the calling function if or not the entries were added.
         """
+        sleep(1)
         for i in range(20):
             stdout.write(f'\rWaiting on DNS propagation time. Remaining: {20 - i}s')
             sleep(1)
         stdout.write('\r')
-
-        # todo: Replicate the following for Windows
         try:
             output = check_output(f"ssh-keyscan {public_ip}", shell=True, stderr=STDOUT).decode('utf-8').split('\n')
         except CalledProcessError as error:
@@ -378,8 +448,93 @@ class VPNServer:
                     self.logger.info('Added required entries to known hosts file.')
                     return True
 
+    def _configure_vpn(self, dns_name: str) -> None:
+        """Configure the VPN server automatically by running a couple of SSH commands and finally a password reset.
+
+        See Also:
+            Takes ~2 minutes as there a wait time for each ``stdin`` in the interactive SSH command.
+
+        Args:
+            dns_name: Takes the public ``DNSName`` as an argument to form the ``ssh`` command to initiate configuration.
+        """
+        self.logger.info('Configuring VPN server.')
+        initial_config = f'ssh -i {self.key_name}.pem root@{dns_name}'
+        final_config = initial_config.replace('root@', 'openvpnas@')
+        vpn_password = environ.get('vpn_password', 'awsVPN2021')
+        script = f"""osascript -e '
+tell application "Terminal"
+    delay 5
+    set currentTab to do script ("cd {getcwd()}")
+    delay 2
+    do script ("{initial_config}") in currentTab
+    delay 10
+    do script ("yes") in currentTab
+    delay 15
+    do script ("yes") in currentTab
+    delay 1
+    do script ("") in currentTab
+    delay 1
+    do script ("") in currentTab
+    delay 1
+    do script ("") in currentTab
+    delay 1
+    do script ("") in currentTab
+    delay 1
+    do script ("") in currentTab
+    delay 1
+    do script ("") in currentTab
+    delay 1
+    do script ("") in currentTab
+    delay 1
+    do script ("") in currentTab
+    delay 1
+    do script ("") in currentTab
+    delay 1
+    do script ("") in currentTab
+    delay 40
+    do script ("{final_config}") in currentTab
+    delay 20
+    do script ("sudo passwd openvpn") in currentTab
+    delay 3
+    do script ("{vpn_password}") in currentTab
+    delay 2
+    do script ("{vpn_password}") in currentTab
+    delay 2
+end tell
+'
+"""
+        script_status = system(script)
+        data = self._retrieve_server_info()
+        url = f"https://{data.get('public_ip')}"
+        if script_status == 256:
+            write_login_details = False
+            self.logger.error('Failed to configure VPN server. '
+                              'Run the below commands following the instructions in README.')
+            self.logger.error(initial_config)
+            self.logger.error(final_config)
+            self.logger.error('sudo passwd openvpn')
+        else:
+            write_login_details = True
+            self.logger.info('VPN server has been configured successfully.')
+            self.logger.info(f"Login Info:\nSERVER: {url}:943/admin/\n"
+                             "USERNAME: openvpn\n"
+                             f"PASSWORD: {vpn_password}\n")
+            self.logger.info('Step1: Now login to the server with the information above and accept the agreement.')
+            self.logger.info('Step2: Navigate to `CONFIGURATION` -> `VPN Settings` and Scroll Down to `Routing`.')
+            self.logger.info('Step3: Slide `Should client Internet traffic be routed through the VPN?` switch to `Yes`')
+            self.logger.info('Step4: Click `Save Settings` (bottom of page) and `Update Running Server` (top of page)')
+        data.update({'initial_config': initial_config, 'final_config': final_config})
+        if write_login_details:
+            data.update({'SERVER': f"{url}:943/admin/", 'USERNAME': 'openvpn', 'PASSWORD': vpn_password})
+        with open(self.server_file, 'w') as file:
+            dump(data, file, indent=2)
+
     def startup_vpn(self) -> None:
-        """Calls the class methods ``_create_ec2_instance`` and ``_instance_info`` to configure the VPN server."""
+        """Calls the class methods ``_create_ec2_instance`` and ``_instance_info`` to configure the VPN server.
+
+        See Also:
+            There is a waiter for 30 seconds for the SSH origin to become active.
+        """
         if instance_basic := self._create_ec2_instance():
             instance_id, security_group_id = instance_basic
         else:
@@ -399,24 +554,31 @@ class VPNServer:
         with open(self.server_file, 'w') as file:
             dump(instance_info, file, indent=2)
 
-        if not self._add_host_entry(public_ip=public_ip, public_dns=public_dns):
-            print("Enter `yes` when prompted to allow the PEM file when running the following command.")
-
         self.logger.info(f'Restricting wide open permissions to {self.key_name}.pem')
         system(f'chmod 400 {self.key_name}.pem')
 
-        config_command = f'ssh -i {self.key_name}.pem root@{public_dns}'
-        print(config_command)  # base command to configure OpenVPN
-        print(config_command.replace('root@', 'openvpnas@'))  # setup OpenVPN using the user id
-        print('sudo passwd openvpn')
+        sleep(1)
+        for i in range(30):
+            stdout.write(f'\rWaiting for SSH origin to be active to ensure successful connection. Remaining: {30 - i}s')
+            sleep(1)
+        stdout.write('\r')
+
+        self._configure_vpn(dns_name=public_dns)
 
     def shutdown_vpn(self) -> None:
-        """Disables VPN server by terminating the ``EC2`` instance, ``KeyPair``, and the ``SecurityGroup`` created."""
-        self._delete_key_pair()
-        self._terminate_ec2_instance()
-        self._delete_security_group()
-        if path.exists(self.server_file):
-            system(f'rm {self.server_file}')
+        """Disables VPN server by terminating the ``EC2`` instance, ``KeyPair``, and the ``SecurityGroup`` created.
+
+        See Also:
+            There is a waiter for 180 seconds for the instance to terminate before deleting the security group.
+        """
+        if self._delete_key_pair() and self._terminate_ec2_instance():
+            sleep(1)
+            for i in range(180):
+                stdout.write(f'\rWaiting for dependent objects before deleting SecurityGroup: {180 - i}s')
+                sleep(1)
+            stdout.write('\r')
+            if self._delete_security_group() and path.exists(self.server_file):
+                system(f'rm {self.server_file}')
 
 
 if __name__ == '__main__':
